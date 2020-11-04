7b38a473cba251b2977dec826ed27545b7c5d3b3
Carmelo <carnamuciocarmelo@gmail.com>
2020-11-04 18:09:52 +0100

    fix - esercizio completato

--- js/script.js
+++ js/script.js
@@ -1,11 +1,17 @@
 $(document).ready(function () {
 
-    $('.with-dropdown').click( function () {
-      $('.dropdown').removeClass('active');
-      $(this).children('with-dropdown').addClass('active');
-    });
 
     $('.with-dropdown').click( function () {
       $(this).children('.dropdown').toggleClass('active');
     });
+
+    $('.with-dropdown').click( function () {
+      if ($(this).children(".dropdown").hasClass("active")){
+        $('.dropdown').removeClass('active');
+        $(this).children('.dropdown').addClass('active');
+      }
+
+    });
+
+
 });
